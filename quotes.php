97847e2199e0ffad380196d322fbadc007994f96
SeregaLSA <vip.sergeylsa@mail.ru>
2023-06-11 23:23:40 +0600

    quotes

--- quotes.php
+++ quotes.php
@@ -1,15 +1,25 @@
 <?php
 require 'functions.php';
 
-$language = [
-    'Английский',
-    'Французкий',
-    'Немецкий',
+$topics = [
+    'Мотивация и вдохновение',
+    'Любовь и отношения',
+    'Мудрость и философия',
+    'Цель и успех',
+    'Жизнь и счастье',
+    'Творчество и искусство',
 ];
 
 
-if (isset($_POST['lang']) && isset($_POST['words'])) {
-    $prompt = 'Переведид текст'. $_POST['words'] . 'с русского на ' . $_POST['lang'];
+if(isset($_POST['words'])){
+    $word = $_POST['words'];
+} else {
+    $word = "";
+}
+
+if (isset($_POST['topic'])) {
+    $prompt = 'Сгенирируй цитату по теме '. $_POST['topic'] . ' с ключевыми словами ' . $word;
+    //print_r($prompt); day();
 
     $answer = chatgpt($prompt);
 }
@@ -21,7 +31,7 @@ if (isset($_POST['lang']) && isset($_POST['words'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Генератор отзывов</title>
+    <title>Генератор цитат</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
@@ -32,16 +42,16 @@ if (isset($_POST['lang']) && isset($_POST['words'])) {
             <div class="col">
                 <form class="mt-3" method="POST">
                     <div class="mb-3">
-                        <select class="form-select" name="lang" require>
-                            <option selected disabled>Выберите на какой язык требуется перевод</option>
-                            <?php foreach($language as $lang) : ?>
-                                <option <?= ($_POST['lang'] ?? '') == $lang ? 'selected' : '' ?>><?= $lang ?></option>
+                        <select class="form-select" name="topic" require>
+                            <option selected disabled>Выберите тему для создания цитаты</option>
+                            <?php foreach($topics as $topic) : ?>
+                                <option <?= ($_POST['topic'] ?? '') == $topic ? 'selected' : '' ?>><?= $topic ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
                     
                     <div class="mb-3">
-                        <label for="words" class="form-label">Исходный текст</label>
+                        <label for="words" class="form-label">Возможные ключевые слова для цитаты</label>
                         <textarea rows="4" class="form-control" id="words" name="words"><?= $_POST['words'] ?? '' ?></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Отправить</button>
